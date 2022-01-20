a9905b2d7b5feb236fecdf277f37dd39a3317c3a
szabotudor <szabo.tudor@gmail.com>
2022-01-20 16:17:43 +0200

    Added possibility to change blend mode

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -1,6 +1,6 @@
 #include<Texture.h>
 
-ss::Texture::Texture(SDL_Window* window, const char* texture) {
+ss::Texture::Texture(SDL_Window* window, const char* texture, SDL_BlendMode blend_mode) {
 	Texture::window = window;
 	render = SDL_GetRenderer(window);
 	format = SDL_AllocFormat(SDL_GetWindowPixelFormat(window));
@@ -15,9 +15,10 @@ ss::Texture::Texture(SDL_Window* window, const char* texture) {
 	clip_rect.w = 0;
 	clip_rect.h = 0;
 	SDL_FreeSurface(sfc);
+	SDL_SetTextureBlendMode(Texture::texture, blend_mode);
 }
 
-ss::Texture::Texture(SDL_Window* window, Vector size) {
+ss::Texture::Texture(SDL_Window* window, Vector size, SDL_BlendMode blend_mode) {
 	Texture::window = window;
 	render = SDL_GetRenderer(window);
 	format = SDL_AllocFormat(SDL_GetWindowPixelFormat(window));
@@ -29,6 +30,7 @@ ss::Texture::Texture(SDL_Window* window, Vector size) {
 	clip_rect.y = 0;
 	clip_rect.w = 0;
 	clip_rect.h = 0;
+	SDL_SetTextureBlendMode(Texture::texture, blend_mode);
 }
 
 void ss::Texture::draw() {
@@ -54,7 +56,7 @@ void ss::Texture::set_pixel(Vector pixel, int r, int g, int b, int a) {
 		return;
 	}
 	else {
-		pixels[(int)pixel.x + (int)pixel.y * (int)resolution.x] = SDL_MapRGB(format, r, g, b);
+		pixels[(int)pixel.x + (int)pixel.y * (int)resolution.x] = SDL_MapRGBA(format, r, g, b, a);
 	}
 }
 
@@ -68,10 +70,11 @@ SDL_Color ss::Texture::get_pixel(Vector pixel) {
 		color.r = 0;
 		color.g = 0;
 		color.b = 0;
+		color.a = 255;
 		return color;
 	}
 	else {
-		SDL_GetRGB(pixels[(int)pixel.x + (int)pixel.y * (int)resolution.x], format, &color.r, &color.g, &color.b);
+		SDL_GetRGBA(pixels[(int)pixel.x + (int)pixel.y * (int)resolution.x], format, &color.r, &color.g, &color.b, &color.a);
 		return color;
 	}
 }
